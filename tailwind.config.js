ab27b80558c1d1e702bd98d14c8b74e1784b1245
rez06 <pelobello.rezyl@gmail.com>
2022-12-24 00:58:31 +0800

    fix some errors

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,16 +11,17 @@ module.exports = {
       lg: '976px',
       xl: '1440px'
     },
+    colors: {
+      primaryClr: 'hsl(25, 97%, 53%)',
+      neutralClr300: 'hsl(0, 0%, 100%)',
+      neutralClr400: 'hsl(217, 12%, 63%)',
+      neutralClr500: 'hsl(216, 12%, 54%)',
+      neutralClr600: 'hsl(213, 19%, 18%)',
+      neutralClr700: 'hsl(216, 12%, 8%)',
+      neutralClr800: 'hsl(210, 18%, 14%)',
+    },
     extend: {
-      colors: {
-        primaryClr: 'hsl(25, 97%, 53%)',
-        neutralClr300: 'hsl(0, 0%, 100%)',
-        neutralClr400: 'hsl(217, 12%, 63%)',
-        neutralClr500: 'hsl(216, 12%, 54%)',
-        neutralClr600: 'hsl(213, 19%, 18%)',
-        neutralClr700: 'hsl(216, 12%, 8%)',
-        neutralClr800: 'hsl(210, 18%, 14%)',
-      },
+
     },
   },
   plugins: [],
